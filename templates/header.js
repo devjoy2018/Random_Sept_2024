78f60d257a3d606f32debab13312e7f93db198e5
Joydeep Dev <dev.joydeep2018@gmail.com>
2024-09-04 19:54:19 +0530

    align/animate/mob.

--- templates/header.js
+++ templates/header.js
@@ -2,11 +2,13 @@ const headerElement = document.createElement("template");
 
 headerElement.innerHTML = `
 <div class="header">
-  <div class="logo">Random September</div>
-  <div class="menu">
-    <a href="#">About Me</a>
-    <a href="#">Contact Me</a>
-    <a href="#">Blog Page</a>
+  <div class="header-el">
+    <div class="logo"><p>Random September</p></div>
+    <div class="menu">
+      <a href="#">About</a>
+      <a class="contact"  href="#">Contact</a>
+      <a class="text" href="#">Blog Page</a>
+    </div>
   </div>
 </div>
 `;
