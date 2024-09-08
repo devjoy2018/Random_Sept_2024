86ed83dd4df2c3e00a378e62738f3976974f0657
Joydeep Dev <dev.joydeep2018@gmail.com>
2024-09-08 12:09:14 +0530

    tweaks

--- templates/footer.js
+++ templates/footer.js
@@ -2,7 +2,7 @@ const footerElement = document.createElement("template-footer");
 
 footerElement.innerHTML = `
 <div class="footer">
-    <p>&copy; 2024 My Website. All rights reserved.</p>
+    <p>&copy; 2024 Ramdom September. All rights reserved.</p>
 <div/>
 `;
 
